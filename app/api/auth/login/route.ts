cecbb741b1882091aa8af59458d30ae3bc448f45
Alef oliveira <alefolivfeira@gmail.com>
2025-04-11 14:52:37 -0300

    fix: chanfe to force-dynamic

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,4 +1,5 @@
-export const dynamic = 'force-static';
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 import { type NextRequest, NextResponse } from "next/server"
 import { authenticateUser } from "@/lib/users"
